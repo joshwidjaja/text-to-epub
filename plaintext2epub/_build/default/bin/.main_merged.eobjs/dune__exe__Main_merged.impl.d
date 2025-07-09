b4571ee663d22fff35031ff3e07f0e5f3904b298
joshwidjaja <josh@widjaja.us>
2025-07-09 20:25:20 +0900

    Adjusted main to use modules

--- plaintext2epub/_build/default/bin/.main_merged.eobjs/dune__exe__Main_merged.impl.d
+++ plaintext2epub/_build/default/bin/.main_merged.eobjs/dune__exe__Main_merged.impl.d
@@ -1,1 +1,1 @@
-bin/main_merged.ml:
+bin/main_merged.ml: Arg Buffer Filename In_channel List Omd Printf Random Str String Sys Unix Uuidm
--- plaintext2epub/_build/default/bin/main_merged.ml
+++ plaintext2epub/_build/default/bin/main_merged.ml
@@ -46,7 +46,7 @@ module type SOURCE_WITH_SPLIT = sig
   val to_xhtml: source -> string
 end
 
-module PlainText: SOURCE_WITH_SPLIT = struct
+module PlainText: SOURCE_WITH_SPLIT with type source = string list = struct
   type source = string list
 
   (* split at \f (form-feed) *)
@@ -63,7 +63,7 @@ module PlainText: SOURCE_WITH_SPLIT = struct
   let to_xhtml lines = xhtml_of_lines lines
 end
 
-module Markdown: SOURCE_WITH_SPLIT = struct
+module Markdown: SOURCE_WITH_SPLIT with type source = string = struct
   type source = string
 
   (* split at H1 headings *)
@@ -88,6 +88,12 @@ module Markdown: SOURCE_WITH_SPLIT = struct
 end
 
 module GenEPub (S: SOURCE_WITH_SPLIT) = struct
+  let get_time () =
+    let tm = Unix.gmtime (Unix.time ()) in
+    Printf.sprintf "%04d-%02d-%02dT%02d:%02d:%02dZ"
+      (tm.tm_year + 1900) (tm.tm_mon + 1) tm.tm_mday
+      tm.tm_hour tm.tm_min tm.tm_sec
+      
   let build ~(title: string) ~(outfile: string) (doc: S.source) =
     (* --- 2. Setup temp dir --- *)
     let parent_dir = Filename.dirname (Sys.getcwd ()) in
@@ -123,16 +129,14 @@ module GenEPub (S: SOURCE_WITH_SPLIT) = struct
       List.mapi
         (fun idx _ ->
           let href = Printf.sprintf "chapter%02d.xhtml" (idx + 1) in
-          Printf.sprintf "<li><a href=\"%s\">Chapter&nbsp;%d</a></li>"
+          Printf.sprintf "<li><a href=\"%s\">Chapter&#160;%d</a></li>"
                           href (idx + 1))
         chapters
       |> String.concat "\n"
     in
     write (Filename.concat oebps "nav.xhtml")
-      (Printf.sprintf {|
-      <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+      (Printf.sprintf {|<?xml version="1.0" encoding="utf-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
   <head><title>%s</title></head>
   <body>
     <nav epub:type="toc" id="toc">
@@ -147,104 +151,85 @@ let uuid =
   let g = Uuidm.v4_gen (Random.State.make_self_init ()) in
   Uuidm.to_string (g ())
 in
-let manifest, spine
+let manifest, spine =
+  List.mapi
+    (fun idx _ ->
+      let id = Printf.sprintf "c%02d" (idx + 1) in
+      let href = Printf.sprintf "chapter%02d.xhtml" (idx + 1) in
+      (Printf.sprintf
+        {|<item id="%s" href="%s" media-type="application/xhtml+xml"/>|} id href,
+      Printf.sprintf {|<itemref idref="%s"/>|} id))
+    chapters
+  |> List.split
+in
+let opf =
+  Printf.sprintf {|<?xml version="1.0" encoding="utf-8"?>
+<package version="3.0"
+         xmlns="http://www.idpf.org/2007/opf"
+         unique-identifier="bookid">
+  <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <dc:identifier id="bookid">urn:uuid:%s</dc:identifier>
+    <dc:title>%s</dc:title>
+    <dc:language>en</dc:language>
+    <meta property="dcterms:modified">%s</meta>
+  </metadata>
+  <manifest>
+%s
+    <item id="nav" href="nav.xhtml"
+          media-type="application/xhtml+xml" properties="nav"/>
+  </manifest>
+  <spine>
+%s
+  </spine>
+</package>|}
+  uuid title
+  (get_time ())
+  (String.concat "\n" manifest)
+  (String.concat "\n" spine)
+in
+write (Filename.concat oebps "content.opf") opf;
+
+(* --- 5. Zip + validate --- *)
+let abs_out = Filename.concat (Sys.getcwd ()) outfile in
+run (Printf.sprintf
+      "cd %s && \
+      zip -X0 %s mimetype && \
+      zip -r9D %s META-INF OEBPS"
+      tmp abs_out abs_out);
+
+run (Printf.sprintf "epubcheck %s" abs_out);
 end
 
 let () =
   (* --- 1. CLI parsing --- *)
   let infile = ref "" in
   let outfile = ref "book.epub" in
+  let title = ref "Untitled" in
   let speclist = [
-    ("-o", Arg.Set_string outfile, "Output EPUB name (default: book.epub)");
+    "-o", Arg.Set_string outfile, "Output EPUB name (default: book.epub)";
+    "-t", Arg.Set_string title, "Book title (default: Untitled)"
   ] in
-  Arg.parse speclist (fun s -> infile := s) "usage: plaintext2epub [-o out.epub] input.(txt|md)";
-  if !infile = "" then (Arg.usage speclist "missing input"; exit 1);
+  let usage = "Usage: text2epub [options] input{txt,md}" in
+  Arg.parse speclist (fun s -> infile := s) usage;
+  if !infile = "" then (Arg.usage speclist usage; exit 1);
 
-  (* --- 2. Setup temp dir --- *)
-  let parent_dir = Filename.dirname (Sys.getcwd ()) in
-  let temp = Filename.concat parent_dir "pte_build" in
-  let cmd = Printf.sprintf "rm -rf %s && mkdir -p %s/META-INF %s/OEBPS" temp temp temp in
-  run cmd;
-
-  (* --- 3. Read plaintext & generate chapter xhtml --- *)
-  let chapter_xhtml =
-    match String.lowercase_ascii (Filename.extension !infile) with
-    | ".md" ->
-      let md = In_channel.with_open_bin !infile In_channel.input_all in
-      xhtml_of_md md
-    | _ ->
-      let ic = open_in_bin !infile in
-      let rec read_lines acc = 
-        match input_line ic with
-        | line -> read_lines (line :: acc)
-        | exception End_of_file -> close_in ic; List.rev acc
-      in
-      xhtml_of_lines (read_lines [])
-
-  in
-  write (temp ^ "/OEBPS/chapter01.xhtml") chapter_xhtml;
-
-  (* --- 4. Static support files --- *)
-  write (temp ^ "/mimetype") "application/epub+zip";
-  write (temp ^ "/META-INF/container.xml") 
-  {|<?xml version="1.0" encoding="UTF-8"?>
-  <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
-  <rootfiles>
-    <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
-  </rootfiles>
-  </container>|};
-  
-  write (temp ^ "/OEBPS/content.opf")
-  {|<?xml version="1.0" encoding="UTF-8"?>
-  <package version="3.0"
-        xmlns="http://www.idpf.org/2007/opf"
-        xmlns:dc="http://purl.org/dc/elements/1.1/"
-        xmlns:dcterms="http://purl.org/dc/terms/"
-        unique-identifier="bookid">
-    <metadata>
-        <dc:identifier id="bookid">urn:uuid:12345678-1234-1234-1234-123456789abc</dc:identifier>
-        <dc:title>Output</dc:title>
-        <dc:language>en</dc:language>
-        <meta property="dcterms:modified">2025-06-12T03:15:00Z</meta>
-    </metadata>
-
-    <manifest>
-        <item id="nav" properties="nav" href="nav.xhtml" media-type="application/xhtml+xml"/>
-        <item id="chapter1" href="chapter01.xhtml" media-type="application/xhtml+xml"/>
-        <item id="css" href="stylesheet.css" media-type="text/css"/>
-    </manifest>
-
-    <spine page-progression-direction="ltr">
-        <itemref idref="chapter1"/>
-    </spine>
-  </package>|};
-
-  write (temp ^ "/OEBPS/nav.xhtml")
-  {|<?xml version="1.0" encoding="UTF-8"?>
-  <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en">
-      <head>
-        <title>Contents</title>
-          <meta charset="UTF-8"/>
-          <meta name="viewport" content="width=device-width"/>
-      </head>
-      <body>
-          <nav epub:type="toc" id="toc">
-              <h2>Contents</h2>
-              <ol>
-                  <li><a href="chapter01.xhtml">Test</a></li>
-              </ol>
-          </nav>
-      </body>
-  </html>|};
-
-  write (temp ^ "/OEBPS/stylesheet.css") ("");
-
-
-  (* --- 5. Zip + validate --- *)
-  let cwd = Sys.getcwd () in
-  Sys.chdir temp;
-  run (sprintf "zip -X0 ../%s mimetype" !outfile);
-  run (sprintf "zip -Xr9D ../%s *" !outfile);
-  run (sprintf "epubcheck ../%s" !outfile);
-  Sys.chdir cwd;
-  printf "Wrote %s\n%!" !outfile
+  (* --- 2. Check for file type --- *)
+  if Filename.check_suffix !infile ".md" then begin
+    let raw_md =
+      In_channel.with_open_bin !infile In_channel.input_all
+    in
+    let module B = GenEPub (Markdown) in
+    B.build ~title:!title ~outfile:!outfile raw_md
+  end else begin
+    let ic = open_in !infile in
+    let rec gather acc =
+      match input_line ic with
+      | line -> gather (line :: acc)
+      | exception End_of_file ->
+        close_in ic;
+        List.rev acc
+    in
+    let raw_txt = gather [] in
+    let module B = GenEPub (PlainText) in
+    B.build ~title:!title ~outfile:!outfile raw_txt
+  end
